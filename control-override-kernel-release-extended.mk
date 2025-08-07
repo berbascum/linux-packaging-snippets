302fc874f13175fce17ff0d389b93de30d65e817
berbascum <berbascum@ticv.cat>
2025-08-07 17:27:08 +0200

    (control-override) kernel-release-extended: Fix dir name mistmatches when packaging
    More precise replacements for preparing control.in was required,
    causing mistmatches betwueen the package dependencies.
    
    As result, the boot images was not included in the deb package.
    
    This commit fixkes the control.in preparation.
    
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- control-override-kernel-release-extended.mk
+++ control-override-kernel-release-extended.mk
@@ -1,12 +1,18 @@
-# control override rule for custom kernel name
+# control override rule for kernel release extended info
+
+#@HOST_ARCH=$$(uname -m); \
+#if [ "$$HOST_ARCH" = "aarch64" ]; then \
+#fi
 
 override debian/control:
-	#@HOST_ARCH=$$(uname -m); \
-	#if [ "$$HOST_ARCH" = "aarch64" ]; then \
-	#fi
 	sed \
-		-e "s|@VARIANT@-@DEVICE_VENDOR@-@DEVICE_MODEL@|@VARIANT@-@KERNEL_DEVELOPER_NAME@-@DEVICE_VENDOR@-@DEVICE_MODEL@|g" \
-		-e "s|@KERNEL_BASE_VERSION@-@DEVICE_VENDOR@-@DEVICE_MODEL@|@VARIANT@-@KERNEL_DEVELOPER_NAME@-@DEVICE_VENDOR@-@DEVICE_MODEL@|g" \
+		-e '/Source: / s|@VARIANT@-@DEVICE_VENDOR@-@DEVICE_MODEL@|@VARIANT@-@KERNEL_DEVELOPER_NAME@-@DEVICE_VENDOR@-@DEVICE_MODEL@|g' \
+		-e '/Vcs-Browser: / s|@VARIANT@-@DEVICE_VENDOR@-@DEVICE_MODEL@|@VARIANT@-@KERNEL_DEVELOPER_NAME@-@DEVICE_VENDOR@-@DEVICE_MODEL@|g' \
+		-e '/Vcs-Git: / s|@VARIANT@-@DEVICE_VENDOR@-@DEVICE_MODEL@|@VARIANT@-@KERNEL_DEVELOPER_NAME@-@DEVICE_VENDOR@-@DEVICE_MODEL@|g' \
+		-e 's|^Package: linux-headers-@DEVICE_VENDOR@-@DEVICE_MODEL@|Package: linux-headers-@VARIANT@-@KERNEL_DEVELOPER_NAME@-@DEVICE_VENDOR@-@DEVICE_MODEL@|g' \
+		-e 's|^Package: linux-bootimage-@DEVICE_VENDOR@-@DEVICE_MODEL@|Package: linux-bootimage-@VARIANT@-@KERNEL_DEVELOPER_NAME@-@DEVICE_VENDOR@-@DEVICE_MODEL@|g' \
+		-e 's|^Package: linux-image-@DEVICE_VENDOR@-@DEVICE_MODEL@|Package: linux-image-@VARIANT@-@KERNEL_DEVELOPER_NAME@-@DEVICE_VENDOR@-@DEVICE_MODEL@|g' \
+		-e 's|@KERNEL_BASE_VERSION@-@DEVICE_VENDOR@-@DEVICE_MODEL@|@KERNEL_BASE_VERSION@-@VARIANT@-@KERNEL_DEVELOPER_NAME@-@DEVICE_VENDOR@-@DEVICE_MODEL@|g' \
 		-e "s|@KERNEL_BASE_VERSION@|$(KERNEL_BASE_VERSION)|g" \
 		-e "s|@VARIANT@|$(VARIANT)|g" \
 		-e "s|@KERNEL_DEVELOPER_NAME@|$(KERNEL_DEVELOPER_NAME)|g" \
@@ -19,5 +25,8 @@ override debian/control:
 		-e "s|@DEB_BUILD_FOR@|$(DEB_BUILD_FOR)|g" \
 	/usr/share/linux-packaging-snippets/control.in > debian/control \
 	&& sed \
-		-e 's/^KERNEL_RELEASE = $(KERNEL_BASE_VERSION)-$(DEVICE_VENDOR)-$(DEVICE_MODEL)/KERNEL_RELEASE = $(KERNEL_BASE_VERSION)-$(VARIANT)-$(KERNEL_DEVELOPER_NAME)-$(DEVICE_VENDOR)-$(DEVICE_MODEL)-$(KBUILD_DROIDIAN_VERSION)/g' \
+	-i 's/^KERNEL_RELEASE = .*/KERNEL_RELEASE = $$(KERNEL_BASE_VERSION)-$$(VARIANT)-$$(KERNEL_DEVELOPER_NAME)-$$(DEVICE_VENDOR)-$$(DEVICE_MODEL)-$$(KBUILD_DROIDIAN_VERSION)/g' \
+	/usr/share/linux-packaging-snippets/kernel-snippet.mk \
+	&& sed \
+		-i '/^kernel_snippet_install:/i kernel_snippet_install: KERNEL_RELEASE = $$(KERNEL_BASE_VERSION)-$$(VARIANT)-$$(KERNEL_DEVELOPER_NAME)-$$(DEVICE_VENDOR)-$$(DEVICE_MODEL)' \
 	/usr/share/linux-packaging-snippets/kernel-snippet.mk
