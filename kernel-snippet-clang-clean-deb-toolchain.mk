78823e12d0e18764ad135a3b02a1e9ded27947e7
berbascum <berbascum@ticv.cat>
2025-08-08 14:42:30 +0200

    (clang) clean-deb-toolchain: Add arch exceptions
    When CLANG_CUSTOM is disabled, only non amd64 hosts does the clean
    !amd64 removes only -4.9- packages
    !amd64 fixes commas and spaces at begin/end
    
    When CLANG_CUSTOM is enabled all hosts does the clean but:
    amd64 only removes clang- packages
    !amd64 removes clang- and -4.9- packages
    all-hosts fixes commas and spaces at begin/end
    
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- kernel-snippet-clang-clean-deb-toolchain.mk
+++ kernel-snippet-clang-clean-deb-toolchain.mk
@@ -5,11 +5,23 @@
 # This causes compilation crashes on other archs, like arm64
 # An example are the android gcc/binutils packages, so we will clean then for non amd64 hosts
 
-# Clean DEB_TOOLCHAIN only when not amd64 host
+# For CLANG_CUSTOM = 1, we need remove clang from DEB_TOOLCHAIN
+# For non amd64 hosts, also gcc 4.9 packages meed to be removed
+
+# For CLANG_CUSTOM = "not 1", we need clean only on non amd64
+
+# Clean DEB_TOOLCHAIN for any host when CLANG_CUSTOM is enabled
+ifeq ($(CLANG_CUSTOM),1)
 ifneq ($(DEB_BUILD_ON),amd64)
 DEB_TOOLCHAIN_CLEANED := $(shell echo $(DEB_TOOLCHAIN) \
 	| tr ' ' '\n' \
 	| grep -v '\-4.9-' \
+	| grep -v '\clang-' \
+	| tr '\n' ' ')
+#
+else ifeq ($(DEB_BUILD_ON),amd64)
+DEB_TOOLCHAIN_CLEANED := $(shell echo $(DEB_TOOLCHAIN) \
+	| tr ' ' '\n' \
 	| grep -v '\clang-' \
 	| tr '\n' ' ')
 #
@@ -19,4 +31,23 @@ DEB_TOOLCHAIN_CLEANED := $(strip $(DEB_TOOLCHAIN_CLEANED))
 DEB_TOOLCHAIN_CLEANED := $(shell echo $(DEB_TOOLCHAIN_CLEANED) | sed 's/^,//; s/,$$//')
 # Remove again possible spaces/tabs from begin and end
 DEB_TOOLCHAIN_CLEANED := $(strip $(DEB_TOOLCHAIN_CLEANED))
+endif
+endif
+
+# Clean DEB_TOOLCHAIN for non amd64 hosts when CLANG_CUSTOM is not enabled
+ifneq ($(CLANG_CUSTOM),1)
+# Clean DEB_TOOLCHAIN only when not amd64 host
+ifneq ($(DEB_BUILD_ON),amd64)
+DEB_TOOLCHAIN_CLEANED := $(shell echo $(DEB_TOOLCHAIN) \
+	| tr ' ' '\n' \
+	| grep -v '\-4.9-' \
+	| tr '\n' ' ')
+#
+# Remove possible spaces/tabs from begin and end
+DEB_TOOLCHAIN_CLEANED := $(strip $(DEB_TOOLCHAIN_CLEANED))
+# Ensure there are not commas at the begin/end
+DEB_TOOLCHAIN_CLEANED := $(shell echo $(DEB_TOOLCHAIN_CLEANED) | sed 's/^,//; s/,$$//')
+# Remove again possible spaces/tabs from begin and end
+DEB_TOOLCHAIN_CLEANED := $(strip $(DEB_TOOLCHAIN_CLEANED))
+endif
 endif
