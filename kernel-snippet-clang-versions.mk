680bff8de8e75a374793e6ca6befafc569c12d58
berbascum <berbascum@ticv.cat>
2025-08-08 22:10:55 +0200

    (snippet) clang-version: Improvements, fixes, comments, debug messages
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- kernel-snippet-clang-versions.mk
+++ kernel-snippet-clang-versions.mk
@@ -1,24 +1,42 @@
-# Clang version configuration
-# Possible supplied integer or string version
+# Clang version configuration snippet
+#
+# For CLANG_CUSTOM not enabled and amd64/arm64 hosts:
+# Set CLANG_VERSION to a supported version using an int.
 
-CLANG_VERSIONS_DROIDIAN := \
+# For CLANG_CUSTOM enabled and amd64/arm64 hosts:
+# For use the predefined repos; set CLANG_CUSTOM_REVISION to
+# a supported revision code.
+# More info in the kernel-info.mk.example
+
+$(info Loading clang-versions-$(CLANG_OS_DISTRIB) snippet for $(DEB_BUILD_ON) and CLANG_CUSTOM = $(CLANG_CUSTOM))
+
+ifeq ($(CLANG_OS_DISTRIB), droidian)
+CLANG_VERSIONS_DROIDIAN_FULL := \
 	6=6.0-4691093 \
 	9=9.0-r353983c \
 	10=10.0-r370808 \
 	12=12.0-r416183b \
 	14=14.0-r450784d
-VERSION_STRING := $(shell echo $(CLANG_VERSIONS_DROIDIAN) \
+
+CLANG_VERSIONS_DROIDIAN_INT := 6 9 10 12 14
+
+ifneq ($(findstring $(CLANG_VERSION), $(CLANG_VERSIONS_DROIDIAN_INT)), $(CLANG_VERSION))
+$(error Unsupported clang-droidian version in kernel-info.mk. Supported: $(CLANG_VERSIONS_DROIDIAN_INT))
+endif
+
+CLANG_VERSION_STR := $(shell echo $(CLANG_VERSIONS_DROIDIAN_FULL) \
 	| tr ' ' '\n' \
 	| grep "^$(CLANG_VERSION)=" \
 	| cut -d'=' -f2)
-ifneq ($(VERSION_STRING),)
-	CLANG_VERSION_STR := $(VERSION_STRING)
-	CLANG_VERSION_INT := $(CLANG_VERSION)
-else
-	CLANG_VERSION_STR := $(CLANG_VERSION)
-	CLANG_VERSION_INT := $(shell \
-		echo $(CLANG_VERSION) \
-		| awk -F'.' '{print $1}')
-endif
+$(info CLANG_VERSION = $(CLANG_VERSION))
+$(info CLANG_VERSION_STR = $(CLANG_VERSION_STR))
 
-CLANG_VERSIONS_DEBIAN := 9 11 13 14 15 16 17 18 19
+else ifeq ($(CLANG_OS_DISTRIB),debian)
+CLANG_VERSIONS_DEBIAN_FULL := 11 13
+CLANG_VERSIONS_DEBIAN_INT := $(CLANG_VERSIONS_DEBIAN_FULL)
+ifneq ($(findstring $(CLANG_VERSION), $(CLANG_VERSIONS_DEBIAN_INT)), $(CLANG_VERSION))
+$(error Unsupported clang-debian version in kernel-info.mk. Supported: $(CLANG_VERSIONS_DEBIAN_INT))
+endif
+$(info CLANG_VERSION = $(CLANG_VERSION))
+
+endif # CLANG_OS_DISTRIB
