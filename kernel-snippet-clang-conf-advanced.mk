5f152c7d2bdcb9d8dd408c8558811f363194cfb4
berbascum <berbascum@ticv.cat>
2025-08-09 02:42:42 +0200

    (snippet) clang-conf-advanced: improve BUILD_PATH_CLANG_CUSTOM
    Best management for the BUILD_PATH for clang custom
    
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- kernel-snippet-clang-conf-advanced.mk
+++ kernel-snippet-clang-conf-advanced.mk
@@ -18,13 +18,12 @@ $(info CLANG_CUSTOM disabled)
 ifndef CLANG_VERSION
 $(error CLANG_VERSION is required in kernel.info.mk)
 endif
-
+#
 ifeq ($(DEB_BUILD_ON),arm64)
 CLANG_OS_DISTRIB := debian
 include /usr/share/linux-packaging-snippets/kernel-snippet-clang-versions.mk
 # Clean DEB_TOOLCHAIN
 include /usr/share/linux-packaging-snippets/kernel-snippet-clang-clean-deb-toolchain.mk
-# TODO: What to do bith the BUILD_PATH
 BUILD_PATH := /usr/lib/llvm-$(CLANG_VERSION_INT)/bin:$(BUILD_PATH)
 else ifeq ($(DEB_BUILD_ON),amd64)
 CLANG_OS_DISTRIB := droidian
@@ -32,23 +31,35 @@ include /usr/share/linux-packaging-snippets/kernel-snippet-clang-versions.mk
 # Clean DEB_TOOLCHAIN
 include /usr/share/linux-packaging-snippets/kernel-snippet-clang-clean-deb-toolchain.mk
 $(info Configuring BUILD_PATH for amd64...)
-# TODO: PULIR PATH SI HI HA COSES DEFINIDES al kernel-info
 BUILD_PATH := /usr/lib/llvm-android-$(CLANG_VERSION_STR)/bin:$(BUILD_PATH)
 endif # ARCH
 
 # CLANG_CUSTOM enabled
 else ifeq ($(CLANG_CUSTOM), 1)
 $(info CLANG_CUSTOM enabled)
-# TODO: Currently managed by the helper script
-# ifeq ($(DOWNLOAD_CLANG_CUSTOM), 1)
-# BUILD_PATH will be overriden
-# endif
-ifndef BUILD_PATH
-$(error BUILD_PATH should be DEFINED from the caller script, ex. kernel-info.mk)
-endif # BUILD_PATH
-ifeq ($(shell test ! -d "$(BUILD_PATH)" && echo notexist),notexist)
-$(error Wrong BUILD_PATH. A valid path is required when CLANG_CUSTOM = 1)
-endif
+#
+# Check BUILD_PATH_CLANG_CUSTOM
+# When download clang custom is enabled, the var should
+# be autoconfigured in kernel-info.mk
+# But we check it anyway.
+# When download clang custom is is disabled,
+# BUILD_PATH_CLANG_CUSTOM must be manually defined in kernel-snippet.mk
+ifeq ($(DOWNLOAD_CLANG_CUSTOM), 1)
+ifndef BUILD_PATH_CLANG_CUSTOM
+$(error BUILD_PATH_CLANG_CUSTOM not defined. Prepend it to the releng-build-package command)
+endif # BUILD_PATH_CLANG_CUSTOM
+else # DOWNLOAD_CLANG_CUSTOM
+ifndef BUILD_PATH_CLANG_CUSTOM
+$(error BUILD_PATH_CLANG_CUSTOM not defined. Check in kernel-info.mk)
+endif # BUILD_PATH_CLANG_CUSTOM
+ifeq ($(shell test ! -d "$(BUILD_PATH_CLANG_CUSTOM)" && echo notexist),notexist)
+$(error Wrong BUILD_PATH_CLANG_CUSTOM. A valid path is required for clang custom. Check in kernel-info.mk)
+endif # shell test dir
+endif # DOWNLOAD_CLANG_CUSTOM
+#
+$(info BUILD_PATH_CLANG_CUSTOM = $(BUILD_PATH_CLANG_CUSTOM))
+BUILD_PATH := $(BUILD_PATH_CLANG_CUSTOM):$(BUILD_PATH)
+#
 # Clean DEB_TOOLCHAIN for clang custom
 include /usr/share/linux-packaging-snippets/kernel-snippet-clang-clean-deb-toolchain.mk
 #DEB_TOOLCHAIN := $(DEB_TOOLCHAIN_CLEANED)
